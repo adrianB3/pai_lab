69b42f1db4c4ad534eaaec74427d480abdda1127
Bălănescu Adrian <balanescuadrian71@gmail.com>
2018-10-26 17:05:19 +0300

    exemplu2 complet

--- lab2/exemplu2/script.php
+++ lab2/exemplu2/script.php
@@ -1,14 +1,34 @@
 <?php
-    mysql_connect("localhost", "root", "wrappi5nG!@#") or die("Nu se poate conecta la serverul MySql");
-    mysql_select_db("lab_pai") or die("Nu se poate selecta baza de date");
+    mysql_connect("localhost", "root", "wrappi5nG!@#") or die("Eroare la conectarea la baza de date");
+    mysql_select_db("lab_pai") or die("Nu se poate deschide baza de date");
+    
+    $nume = $_POST['nume'];
+    $varsta = $_POST['varsta'];
 
-    //$nume = $_POST['nume'];
-    //$varsta = $_POST['varsta'];
+    $querry = mysql_query("select * from persoane where nume='$nume' and varsta='$varsta'");
 
-    $query = mysql_querry("select * from persoane");
-    $nr_inreg = mysql_num_rows($query);
+    $nr_col = mysql_num_fields($querry);
+    $nr_rows = mysql_num_rows($querry);
 
-    if($nr_inreg > 0){
-        echo "S-au gasit: ".$nr_inreg." inregistrari";
+    echo "<table align=’center’ BORDER=’2’>";
+    echo "<tr>";
+    echo "<th>Nume</th>";
+    echo "<th>Varsta</th>";
+    echo "<th>Localitate</th>";
+    echo "</tr>";
+
+    if($nr_rows > 0){
+        while($row = mysql_fetch_row($querry)){
+            echo "<tr>";
+            foreach($row as $value){
+                echo "<td>$value</td>";
+            }
+            echo "<tr>";
+        }
     }
+    else{
+        die("Nicio inregistrare");
+    }
+
+    mysql_close();
 ?>
