976649ab1f78a05ad06a8a816ba55d87021689ef
Bălănescu Adrian <balanescuadrian71@gmail.com>
2018-10-26 16:46:57 +0300

    ..

--- lab2/exemplu2/script.php
+++ lab2/exemplu2/script.php
@@ -1,14 +1,14 @@
 <?php
     mysql_connect("localhost", "root", "wrappi5nG!@#") or die("Nu se poate conecta la serverul MySql");
-    mysql_select_db("pai_lab") or die("Nu se poate selecta baza de date");
+    mysql_select_db("lab_pai") or die("Nu se poate selecta baza de date");
 
-    $nume = $_POST['nume'];
-    $varsta = $_POST['varsta'];
+    //$nume = $_POST['nume'];
+    //$varsta = $_POST['varsta'];
 
-    $query = mysql_querry("select * from persoane where nume='$nume' and varsta='$varsta'");
+    $query = mysql_querry("select * from persoane");
     $nr_inreg = mysql_num_rows($query);
 
     if($nr_inreg > 0){
-        
+        echo "S-au gasit: ".$nr_inreg." inregistrari";
     }
 ?>
